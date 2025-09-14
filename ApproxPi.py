226f0a334f87e091f27015b22bca971b1bd0de18
Fpyton <fmolina@unomaha.edu>
2025-09-14 05:59:13 +0000

    Done With Pi

--- ApproxPi.py
+++ ApproxPi.py
@@ -12,11 +12,19 @@ def main():
   realPi = math.pi
 
   #ask user for decimal percision (up to 10)
-
+  terms = int(input("Enter the number of terms to approximate pi: "))
   start = time.time()
   #calculate pi using the approximation technique
   #Loop until the level of percision is reached
+  approxPi = 4/1
+  sign = -1
+  denom = 3
+  while round(approxPi, 6) != round(realPi, 6):
+    #print(approxPi)
+    approxPi = approxPi + (sign * 4/denom)
 
+    sign = sign * -1
+    denom = denom + 2
   end = time.time()
 
   elapsedTime = end - start
