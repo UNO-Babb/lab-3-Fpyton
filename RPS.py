7c08f71aca1fb42de7eb6ae82abe6316d5e35172
Fpyton <fmolina@unomaha.edu>
2025-09-14 05:13:54 +0000

    Done with RPS

--- RPS.py
+++ RPS.py
@@ -8,18 +8,63 @@ def main():
   wins = 0
   ties = 0
   losses = 0
-  #Create a loop that continues as long as the user wants to play.
-  #User can play as many games as they wish.
 
-  #Randomly choose the computer between 'R', 'P', or 'S'
-  #Prompt the user for their RPS selection
-  #Determine winner and state what happened to the user
-  #Ask the user if they would like to play again.
+  PlayAgain = "Y"
+  while PlayAgain == "Y":
+    #Create a loop that continues as long as the user wants to play.
+    #User can play as many games as they wish.
+    computer = random.choice( ["R", "R", "R", "P", "S", "S"])
+    player = input("Pick your weapon (R, P, S): ")
+    #Randomly choose the computer between 'R', 'P', or 'S'
+    #Prompt the user for their RPS selection
+    #Determine winner and state what happened to the user
+    if computer == "R" :
+      print("computer chose ROCK")
+    elif computer == "P" :
+      print("computer chose Paper")
+    else:
+      print("computer chose Scissors")
 
+    if player == "R" :
+      print("player chose ROCK")
+    elif player == "P" :
+      print("player chose Paper")
+    else:
+      print("player chose Scissors")
+
+    if player == "R" and computer == "R" :
+      print("Tie")
+      ties = ties + 1
+    if player == "P" and computer == "P" :
+      print("Tie")
+      ties = ties + 1
+    if player == "S" and computer == "S" :
+      print("Tie")
+      ties = ties + 1
+    if player == "R" and computer == "P" :
+      print(" computer wins,")
+      losses = losses + 1
+    if player == "P" and computer == "S" :
+      print("computer wins,")
+      losses = losses + 1
+    if player == "S" and computer == "R" :
+      print("computer wins,")
+      losses = losses + 1
+    if player == "R" and computer == "S" :
+      print("You Win")
+      wins = wins + 1
+    if player == "P" and computer == "R" :
+      print("You Win")
+      wins = wins + 1
+    if player == "S" and computer == "P" :
+      print("You Win")
+      wins = wins + 1
+    #Ask the user if they would like to play again.
+    playAgain = input("Play again? (Y/N): ")
   #In the end, print the stats
-  print("Wins \t Ties \t Losses")
-  print("---- \t ---- \t ------")
-  print(wins, "\t", ties , "\t", losses)
+    print("Wins \t Ties \t Losses")
+    print("---- \t ---- \t ------")
+    print(wins, "\t", ties , "\t", losses)
 
 if __name__ == '__main__':
   main()
